6ad56233fc60485706ee5d3f7759e109fe1a5ff2
Hasmot Ali Hasu <hasmot23@gmail.com>
2020-09-18 02:07:25 +0600

    Update PartTimeEmployee.java

--- Employee Management/src/PartTimeEmployee.java
+++ Employee Management/src/PartTimeEmployee.java
@@ -1,4 +1,14 @@
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hasmot Ali Hasu
+ */
 public class PartTimeEmployee extends Employee {
     private double hourlyRate;
     private int hoursPerWeek;
