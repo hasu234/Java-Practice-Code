717440ca9eec36e74bbb911e40c92866650b66bf
Hasmot Ali Hasu <hasmot23@gmail.com>
2020-09-18 02:07:46 +0600

    Update Test.java

--- Employee Management/src/Test.java
+++ Employee Management/src/Test.java
@@ -1,3 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hasmot Ali Hasu
+ */
 public class Test {
     public static void main(String[] args) {
         PartTimeEmployee m = new PartTimeEmployee("Mehedi","172-188");
