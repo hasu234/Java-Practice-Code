3aec9fc00a243937c6178b891ccff870146268dd
Hasmot Ali Hasu <hasmot23@gmail.com>
2020-09-22 22:28:31 +0600

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package july3.pkg2017;
 
-/**
- *
- * @author Administrator
- */
 public class Faculty extends Employee{
     private double officeHour;
     private String rank;
@@ -21,4 +11,4 @@ public class Faculty extends Employee{
     public String toString(){
        return getClass().getName()+" "+this.name;
     }
-}
+
